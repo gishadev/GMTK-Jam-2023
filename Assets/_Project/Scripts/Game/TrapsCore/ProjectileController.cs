e3e1dc1eb0cd3355646b370440c4dc42e9aeff2e
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-07-09 15:36:38 +0300

    added projecyile trap

---
+++ Assets/_Project/Scripts/Game/TrapsCore/ProjectileController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using GMTK.Game.EnemyCore;
+using UnityEngine;
+
+namespace GMTK.Game.TrapsCore
+{
+    public class ProjectileController : MonoBehaviour
+    {
+        public event Action<ProjectileController> OnProjectileDisabled; 
+
+        [SerializeField] private float projectileSpeed = 3;
+        [SerializeField] private Rigidbody rb;
+
+        private Vector3 _startPosition;
+        private DamageType _damageType;
+
+        private void OnEnable()
+        {
+            rb.velocity = Vector3.back * projectileSpeed;
+        }
+
+        private void OnDisable()
+        {
+            OnProjectileDisabled?.Invoke(this);
+            transform.position = _startPosition;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.TryGetComponent(out IDamageable damageable))
+            {
+                damageable.TakeDamage(_damageType);
+            }
+            
+            gameObject.SetActive(false);
+        }
+
+        public void Init(DamageType damageType)
+        {
+            _damageType = damageType;
+            _startPosition = transform.position;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Game/TrapsCore/ProjectileTrapController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace GMTK.Game.TrapsCore
+{
+    public class ProjectileTrapController : TrapController
+    {
+        [SerializeField] private ProjectileController projectilePrefab;
+        [SerializeField] private Transform[] spawnSpots;
+        
+        [SerializeField] private int projectilesInPool;
+        private Queue<ProjectileController> _projectilePool;
+
+        private void Awake()
+        {
+            _projectilePool = new Queue<ProjectileController>();
+            ProjectileController projectile;
+
+            for (int i = 0; i < projectilesInPool; i++)
+            {
+                projectile = Instantiate(projectilePrefab, spawnSpots[Random.Range(0, spawnSpots.Length)].position, Quaternion.identity);
+                projectile.Init(damageType);
+
+                projectile.OnProjectileDisabled += AddToPool;
+                
+                projectile.gameObject.SetActive(false);
+                AddToPool(projectile);
+            }
+        }
+
+        private void Start()
+        {
+            StartCoroutine(ShootArrowsRecursively());
+        }
+
+        private IEnumerator ShootArrowsRecursively()
+        {
+            yield return new WaitForSeconds(cooldown);
+            _projectilePool.Dequeue().gameObject.SetActive(true); 
+            StartCoroutine(ShootArrowsRecursively());
+        }
+
+        private void AddToPool(ProjectileController projectile)
+        {
+            _projectilePool.Enqueue(projectile);
+        }
+    }
+}
