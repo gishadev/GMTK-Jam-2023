88de1aade6586d2f6b0e69256d90a112c21c6e9a
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-07-09 12:48:01 +0300

    added trigger trap

---
+++ Assets/_Project/Scripts/Game/TrapsCore/TriggerTrapController.cs
@@ -0,0 +1,17 @@
+﻿using GMTK.Game.EnemyCore;
+using UnityEngine;
+
+namespace GMTK.Game.TrapsCore
+{
+    public class TriggerTrapController : MonoBehaviour
+    {
+        [SerializeField] private DamageType damageType;
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out IDamageable damageable))
+            {
+                damageable.TakeDamage(damageType);
+            }
+        }
+    }
+}
