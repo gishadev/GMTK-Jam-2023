d2908be00c410fe67e6a38fd31f97548a83e376a
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-07-09 13:13:55 +0300

    added traps cooldown

--- Assets/_Project/Scripts/Game/TrapsCore/TriggerTrapController.cs
+++ Assets/_Project/Scripts/Game/TrapsCore/TriggerTrapController.cs
@@ -1,4 +1,5 @@
-﻿using GMTK.Game.EnemyCore;
+﻿using System.Collections;
+using GMTK.Game.EnemyCore;
 using UnityEngine;
 
 namespace GMTK.Game.TrapsCore
@@ -6,12 +7,28 @@ namespace GMTK.Game.TrapsCore
     public class TriggerTrapController : MonoBehaviour
     {
         [SerializeField] private DamageType damageType;
+        
+        [SerializeField] private float cooldown;
+        
+        private bool _canUse = true;
+
         public void OnTriggerEnter(Collider other)
         {
-            if (other.TryGetComponent(out IDamageable damageable))
+            if (_canUse)
             {
-                damageable.TakeDamage(damageType);
+                if (other.TryGetComponent(out IDamageable damageable))
+                {
+                    damageable.TakeDamage(damageType);
+                    _canUse = false;
+                    StartCoroutine(Cooldown());
+                }
             }
+        }
+
+        private IEnumerator Cooldown()
+        {
+            yield return new WaitForSeconds(cooldown);
+            _canUse = true;
         }
     }
 }
