ddc8917b090f0e25662f657d9711a870cea5186c
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-07-09 17:18:35 +0300

    added main menu scene

---
+++ Assets/_Project/Scripts/Game/UI/MainMenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GMTK.Game.UI
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        [SerializeField] private string gameScene;
+
+        [SerializeField] private Animation startTextBlink;
+
+        private void Start()
+        {
+            startTextBlink.Play();
+        }
+
+        private void Update()
+        {
+            if (Input.anyKey)
+            {
+                SceneManager.LoadScene(gameScene);
+            }
+        }
+    }
+}
