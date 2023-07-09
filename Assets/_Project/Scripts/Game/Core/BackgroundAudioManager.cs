c18d48de13640112b8f6829466340c7bda264e8b
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-07-09 18:49:33 +0300

    added background music

---
+++ Assets/_Project/Scripts/Game/Core/BackgroundAudioManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace GMTK._Project.Scripts.Game.Core
+{
+    public class BackgroundAudioManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
